4593361336b33e5d5ba6fb699c1303df75b7d69c
Levi Joyce <linuxgamer82@gmail.com>
2024-05-14 12:49:00 -0400

    exercise 2

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> games = new List<string>
+            { "Elden Ring", "Elder Scrolls", "Ancient Domains Of Mystery",
+                "The Doors of Trithius", "Soulash 2",
+                "Stoneshard", "Path of Achra",
+                 "Tales of Maj'Eyal"
+            };
+
+            IEnumerable<string> longGameNames = games.OrderByDescending(x => x.Length);
+
+            foreach (var game in longGameNames)
+            {
+                Console.WriteLine(game);
+            }
+
         }
     }
 }
